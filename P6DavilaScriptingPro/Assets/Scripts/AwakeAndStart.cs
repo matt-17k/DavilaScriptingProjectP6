f71c4e1a2d88b3abee5f6bc5e93328d3347a589b
Matthew <769438@student.dusd.net>
2026-01-08 13:48:52 -0800

    a

---
+++ P6DavilaScriptingPro/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+     void Awake()
+    {
+        Debug.Log("Awake called");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start is running");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
